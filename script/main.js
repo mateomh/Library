9436d6da48a1b2f3ab27623bb8bdd2e55a370bd2
muhenge <mugunga5@gmail.com>
2020-10-05 19:01:57 +0200

    Add js not yet done

--- script/main.js
+++ script/main.js
@@ -1,9 +1,32 @@
+// setting the constructor
+// event for adding, removing
+// localStorage
+
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+const addBook = document.getElementById('add');
+// const titles = document.getElementsByClassName('book-input');
+// const card = document.getElementById('card');
+
+function Book(title,author,pages) {
+  this.title = title
+  this.author = author
+  this.pages = pages
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  viewBooks();
 }
+
+function viewBook() { //display a card on the brawser
+  for (let oneBook in myLibrary) {
+    const cardBook = document.createElement('div');
+    card.classList.add('book-card');
+  }
+}
+
+addBook.addEventListener('click',(event)=>{
+  event.preventDefault
+  
+})
